0458e7353f75f58c2d2de007b99d7aa916590d86
PoorvikaL <lpoorvika16@gmail.com>
2025-09-29 12:55:49 +0530

    implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,6 +12,9 @@ import {toast} from "sonner";
 import FormField from "@/components/FormField";
 // import {useRouter} from "next/router";
 import {useRouter} from "next/navigation";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "@/firebase/client";
+import {signIn, signUp} from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
     return z.object({
@@ -35,12 +38,43 @@ const AuthForm = ({ type } : { type : FormType}) => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
        try {
            if(type === "sign-up") {
+               const { name, email, password } = values;
+
+               const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+
+               const result = await signUp({
+                   uid: userCredentials.user.uid,
+                   name: name!,
+                   email,
+                   password,
+               })
+
+               if(!result?.success) {
+                   toast.error(result?.message);
+                   return;
+               }
                toast.success('Account created successfully.Please sign in.');
                router.push('/sign-in')
            } else {
+
+               const { email, password } = values;
+
+               const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+               const idToken = await userCredential.user.getIdToken();
+
+               if(!idToken) {
+                   toast.error('Sign in failed');
+                   return;
+               }
+
+               await signIn({
+                   email, idToken
+               })
+
                toast.success('sign in successfully.');
                router.push('/')
            }
@@ -61,29 +95,38 @@ const AuthForm = ({ type } : { type : FormType}) => {
                 <h3>Practise job interview with AI</h3>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+                    {/* Show Name field only on Sign Up */}
                     {!isSignIn && (
                         <FormField
                             control={form.control}
-                            name="email"
-                            label="email"
-                            placeholder="Your email address"
-                            type="email"
+                            name="name"
+                            label="Name"
+                            placeholder="Your Name"
                         />
-                        )}
+                    )}
+
+                    {/* Email is always shown */}
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        label="Email"
+                        placeholder="Your email address"
+                        type="email"
+                    />
+
+                    {/* Password is always shown */}
                     <FormField
                         control={form.control}
                         name="password"
-                        label="password"
+                        label="Password"
                         placeholder="Enter your password"
                         type="password"
                     />
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        label="Name"
-                        placeholder="Your Name"
-                    />
-                    <Button className="btn" type="submit">{isSignIn ? 'Sign in' : 'Create an Account'}</Button>
+
+                    <Button className="btn" type="submit">
+                        {isSignIn ? 'Sign in' : 'Create an Account'}
+                    </Button>
+
                 </form>
             </Form>
                 <p className="text-center">
